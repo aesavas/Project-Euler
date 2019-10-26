db441d0f2cdcabf84b938f86e9401ac7cba0eacd
Ali Emre SAVAŞ <aliemresavas.95@gmail.com>
2019-10-26 22:56:16 +0100

    Problem 18(Python code) added.

---
+++ Problem 018/Python/Problem 18.py
@@ -0,0 +1,53 @@
+"""
+author : aesavas
+
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from
+top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67,
+is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires
+a clever method! ;o)
+"""
+import time
+start = time.time()
+file = open("dataForProblem18.txt","r")
+listOfNumbers = file.read().splitlines()
+listOfNumbers = [i.split() for i in listOfNumbers]
+pathOfSum = list() # It collects path of sum
+for i in range(len(listOfNumbers)-1, 0, -1): # -1 means decreasing
+    for j in range(len(listOfNumbers[i])-1, 0, -1):
+        first = int(listOfNumbers[i][j])+int(listOfNumbers[i-1][j-1])
+        second = int(listOfNumbers[i][j-1])+int(listOfNumbers[i-1][j-1])
+        if first > second:
+            listOfNumbers[i-1][j-1] = first
+        else:
+            listOfNumbers[i-1][j-1] = second
+print("Sum of maximum path is: ", listOfNumbers[0][0])
+print("Time : ", time.time() - start)
+
+
